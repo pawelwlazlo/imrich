6b5a21dd501aab02facb0411836d1250669e7507
Pawel Wlazlo <mrogur@gmail.com>
2022-07-14 18:59:44 +0200

    added background card

--- app/src/main/java/dev/wlazlo/imrich/MainActivity.kt
+++ app/src/main/java/dev/wlazlo/imrich/MainActivity.kt
@@ -3,13 +3,17 @@ package dev.wlazlo.imrich
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.wlazlo.imrich.ui.theme.ImRichTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +33,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateImRich() {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
+        Card(modifier = Modifier
+            .width(200.dp)
+            .height(390.dp)
+            .padding(12.dp),
+            shape = RoundedCornerShape(corner = CornerSize(15.dp)),
+            elevation = 4.dp) {
 
+        }
+    }
 }
 
 @Preview(showBackground = true)

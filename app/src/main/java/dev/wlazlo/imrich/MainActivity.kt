742dc2b89ce410bf749d00b862c45c583f09f93c
Pawel Wlazlo <mrogur@gmail.com>
2022-07-14 19:22:32 +0200

    adding column

--- app/src/main/java/dev/wlazlo/imrich/MainActivity.kt
+++ app/src/main/java/dev/wlazlo/imrich/MainActivity.kt
@@ -3,7 +3,10 @@ package dev.wlazlo.imrich
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -12,7 +15,11 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import dev.wlazlo.imrich.ui.theme.ImRichTheme
 
@@ -22,7 +29,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             ImRichTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
                     CreateImRich()
 
                 }
@@ -32,16 +42,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun Logo(size: Dp, padding: Dp) {
+    Surface(
+        shape = CircleShape,
+        modifier = Modifier
+            .size(size)
+            .padding(padding),
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        elevation = 4.dp,
+        color = MaterialTheme.colors.onSurface.copy(
+            alpha = 0.5f
+        )
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.blue_diamond),
+            contentDescription = "Blue diamond",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
 fun CreateImRich() {
-    Surface(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()) {
-        Card(modifier = Modifier
-            .width(200.dp)
-            .height(390.dp)
-            .padding(12.dp),
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Card(
+            modifier = Modifier
+                .width(200.dp)
+                .height(390.dp)
+                .padding(12.dp),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
-            elevation = 4.dp) {
+            elevation = 4.dp
+        ) {
+            Column() {
+                Logo(150.dp, 5.dp)
+            }
 
         }
     }
